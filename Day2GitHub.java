479b89e4de74a74a643a13cbcdbeefd165473a09
Nisarga Shashidhar <45395264+NisargaShashidhar@users.noreply.github.com>
2018-11-29 10:25:03 -0500

    Created convertToArrayList method
    Created convertToArrayList method, which takes an Integer and converts it to an ArrayList. The main method also contains 2 tests for this method.

--- Day2GitHub.java
+++ Day2GitHub.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Day2GitHub{
 	public static void main(String[]args){
-		
+
+		ArrayList<Integer> list = convertToArrayList(8950);
+		System.out.println(list);
+
+		ArrayList<Integer> list2 = convertToArrayList(9071);
+		System.out.println(list2);
+
+	}
+
+	public static ArrayList<Integer> convertToArrayList(int chain){
+		ArrayList<Integer> list = new ArrayList<>();
+		int i = 0;
+		while(chain > 0){
+			i++;
+			list.add(0,chain%10);
+			chain = chain/10;
+		}
+
+		return list;
 	}
 }
